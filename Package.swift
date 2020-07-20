4c235dc43289f1b51f6f138cf4cd5a34a1ae5dfc
eunji98 <eunji98@kissoft.co.kr>
2020-07-20 15:52:56 +0900

    타겟 수정

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             path: "sources",
             sources: ["SDVersion"],
             cSettings: [
-                .headerSearchPath("SDVersion")
+                .headerSearchPath("SDiOSVersion")
             ]
         ),
         .testTarget(
