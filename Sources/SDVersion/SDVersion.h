248ec8ad42f2347a357d2d7444c49c5baf7d4829
eunji98 <eunji98@kissoft.co.kr>
2020-07-21 09:31:44 +0900

    path 수정

--- Sources/SDVersion/SDVersion.h
+++ Sources/SDVersion/SDVersion.h
@@ -4,23 +4,27 @@
 //
 //  Copyright (c) 2016 Sebastian Dobrincu. All rights reserved.
 //
+#import "SDiOSVersion.h"
+#import "SDwatchOSVersion.h"
+#import "SDtvOSVersion.h"
+#import "SDMacVersion.h"
 
 #ifndef SDVersion_h
 #define SDVersion_h
 
-#if TARGET_OS_IOS
-	#import "SDiOSVersion.h"
-	#define SDVersion SDiOSVersion
-#elif TARGET_OS_WATCH
-    #import "SDwatchOSVersion.h"
-    #define SDVersion SDwatchOSVersion
-#elif TARGET_OS_TV
-    #import "SDtvOSVersion.h"
-    #define SDVersion SDtvOSVersion
-#elif TARGET_OS_MAC
-    #import "SDMacVersion.h"
-    #define SDVersion SDMacVersion
-#endif
+//#if TARGET_OS_IOS
+//	#import "SDiOSVersion.h"
+//	#define SDVersion SDiOSVersion
+//#elif TARGET_OS_WATCH
+//    #import "SDwatchOSVersion.h"
+//    #define SDVersion SDwatchOSVersion
+//#elif TARGET_OS_TV
+//    #import "SDtvOSVersion.h"
+//    #define SDVersion SDtvOSVersion
+//#elif TARGET_OS_MAC
+//    #import "SDMacVersion.h"
+//    #define SDVersion SDMacVersion
+//#endif
 
 
 
