c14a69437dc85818e39fbbb8109fa2144c61e006
eunji98 <eunji98@kissoft.co.kr>
2020-07-21 15:45:22 +0900

    -

--- Sources/SDVersion/SDVersion.h
+++ Sources/SDVersion/SDVersion.h
@@ -8,24 +8,20 @@
 #ifndef SDVersion_h
 #define SDVersion_h
 
-#import "SDiOSVersion.h"
-#import "SDwatchOSVersion.h"
-#import "SDtvOSVersion.h"
-#import "SDMacVersion.h"
 
-//#if TARGET_OS_IOS
-//	#import "SDiOSVersion.h"
-//	#define SDVersion SDiOSVersion
-//#elif TARGET_OS_WATCH
-//    #import "SDwatchOSVersion.h"
-//    #define SDVersion SDwatchOSVersion
-//#elif TARGET_OS_TV
-//    #import "SDtvOSVersion.h"
-//    #define SDVersion SDtvOSVersion
-//#elif TARGET_OS_MAC
-//    #import "SDMacVersion.h"
-//    #define SDVersion SDMacVersion
-//#endif
+#if TARGET_OS_IOS
+	#import "SDiOSVersion.h"
+	#define SDVersion SDiOSVersion
+#elif TARGET_OS_WATCH
+    #import "SDwatchOSVersion.h"
+    #define SDVersion SDwatchOSVersion
+#elif TARGET_OS_TV
+    #import "SDtvOSVersion.h"
+    #define SDVersion SDtvOSVersion
+#elif TARGET_OS_MAC
+    #import "SDMacVersion.h"
+    #define SDVersion SDMacVersion
+#endif
 
 
 
