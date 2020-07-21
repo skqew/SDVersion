73e618c2ec0ebfcaee03c26cbb36c3ecd71aeea2
eunji98 <eunji98@kissoft.co.kr>
2020-07-21 15:37:52 +0900

    -

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
             path: "Sources",
             exclude: [],
             sources: ["SDVersion/SDiOSVersion.m", "SDVersion/SDMacVersion.m", "SDVersion/SDtvOSVersion.m", "SDVersion/SDwatchOSVersion.m"],
-            publicHeadersPath: "Sources"
+            publicHeadersPath: "SDVersion.h"
         ),
         .testTarget(
             name: "SDVersionTests",
