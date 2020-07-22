320f98f29bf3dd6371d3bab35a6a9249beb17579
eunji98 <eunji98@kissoft.co.kr>
2020-07-22 11:58:09 +0900

    -

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
         .library(
             name: "SDVersion",
             targets: ["SDVersion"]),
+            .library(
+            name: "SDttttestVersion",
+            targets: ["SDtestVersion"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
