c81fa460dbe19709b22069d799e7cd749c879dd2
eunji98 <eunji98@kissoft.co.kr>
2020-07-22 11:55:45 +0900

    -

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             name: "SDVersion",
             targets: ["SDVersion"]),
             .library(
-            name: "SDtestttttVersion",
+            name: "SDtestVersion",
             targets: ["SDtestVersion"]),
     ],
     dependencies: [
